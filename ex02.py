7a2894c7b67eaec77b76b11cfb22995b0d6f52eb
Michel <106674127+MichelZero@users.noreply.github.com>
2022-11-01 10:11:24 -0300

    up

--- ex02.py
+++ ex02.py
@@ -11,6 +11,7 @@
 # 5! = 120 -> 1*2*3*4*5
 
 ######  1  #######
+""" 
 numero = int(input("informar um valor positivo: "))
 valor = numero
 
@@ -20,3 +21,22 @@ while (numero > 0):
   numero = numero - 1
 
 print(f'o valor [{valor}!] = {fat}')
+"""
+
+
+
+######  2  #######
+# entrada de dados
+numero = int(input("informar um valor positivo: "))
+valor = numero
+fat = 1
+
+# processamento
+if (numero < 0):
+  pass
+elif (numero >= 0):
+  while (numero > 0):
+    fat = fat * numero # ex 5! -> 5 * 4 * ... = 120
+    numero = numero - 1
+  else:
+    print(f'o valor [{valor}!] = {fat}')
